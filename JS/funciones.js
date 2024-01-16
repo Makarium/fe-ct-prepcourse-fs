8d4e22fbfb60f7ca5516e7b6acb4e71d7301e75e
Makarium <miguelsancraft@gmail.com>
2024-01-15 21:58:09 -0500

    Upload de homework

---
+++ JS/funciones.js
@@ -0,0 +1,19 @@
+function sumaTres(x) {
+    console.log(x + 3);
+}
+
+sumaTres(5);
+
+function sumaTres(x) {
+    return x + 3;
+}
+
+var sumarTres = function (x) {
+    return x + 3;
+};
+
+var sumarTres = (x) => {
+    return x + 3;
+};
+
+ 
